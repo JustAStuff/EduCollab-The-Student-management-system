ec24c97a9db609df12e1c2c0ea3cf165fb3f198b
subash k <k850673@gmail.com>
2025-09-15 20:48:42 +0530

    workstart

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Dashboard from "./Dashboard";
 import CreateWorkspace from "./CreateWorkspace";
 import ResetPassword from "./ResetPassword";
 import ForgotPassword from "./ForgotPassword";
-import PublicChat from "./PublicChat";
 function App() {
   return (
     <Routes>
@@ -15,7 +14,7 @@ function App() {
       <Route path="/create-workspace" element={<CreateWorkspace />} />
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="/forgot-password" element={<ForgotPassword/>}/>
-      <Route path="/PublicChat" element={<PublicChat/>}/>
+
     </Routes>
   );
 }
